905b8f4c6dfd04f15b33ccf763f259bee1022171
Micoder-dev <micoder.com@gmail.com>
2022-07-17 23:38:58 +0530

    listView basic contents added by displaying a planets name list

--- app/src/main/java/com/example/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.listviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = findViewById(R.id.my_listview);
+
+        String[] my_planets = new String[] {
+                "Mercury",
+                "Venus",
+                "Earth",
+                "Mars",
+                "Jupiter",
+                "Saturn",
+                "Uranus",
+                "Neptune"
+        };
+
+        ArrayList<String> myPlanetsList = new ArrayList<>();
+        myPlanetsList.addAll(Arrays.asList(my_planets));
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.my_row, myPlanetsList);
+
+        listAdapter.add("Ceres");
+        listAdapter.add("Pluto");
+        listAdapter.add("Haumea");
+        listAdapter.add("Makemake");
+        listAdapter.add("Eris");
+
+        mListView.setAdapter(listAdapter);
+
     }
 }
